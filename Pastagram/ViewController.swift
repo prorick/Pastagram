705c9efe50faa75920cc77ddd2ba75caa618b01a
prorick <56745348+prorick@users.noreply.github.com>
2020-11-16 21:19:54 -0800

    Initial Commit

---
+++ Pastagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pastagram
+//
+//  Created by Prothit Halder on 11/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
