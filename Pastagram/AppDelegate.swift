53d12deefc34d0216ccbd1548a7361a60b973875
prorick <56745348+prorick@users.noreply.github.com>
2020-11-16 23:51:24 -0800

    Pastagram features include user login, posting pictures, and viewing pictures that were posted.

--- Pastagram/AppDelegate.swift
+++ Pastagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "WwLG7mxCe91KvXQwG976kxBoYpJ36Eo95jTpzIop" // <- UPDATE
+                    $0.clientKey = "PvvUxz5E2J5oUoTgd1cQu9ssC66qlhtQnyTRB39z" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+        
+        Parse.initialize(with: parseConfig)
+        
+        
         return true
     }
 
